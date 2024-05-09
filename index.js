116eb2325c4e6628a08698004273045e259fc77d
Francisco Palacios <francisco.palacios@crescendocollective.com>
2024-05-09 14:34:54 -0600

    initial website build

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $(".hero-slider").slick({
+        fade: true,
+        slidesToShow: 1,
+        autoplay: true,
+        dots: true,
+        centerMode: false,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        speed: 1500,
+    });
+});
